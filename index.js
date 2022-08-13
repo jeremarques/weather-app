73800bc4e0e2186d0efcfafc852e2e48c0de20eb
Jeremias Marques <saimerejmarques@gmail.com>
2022-08-13 12:48:15 -0300

    index.js refactoring

--- index.js
+++ index.js
@@ -24,13 +24,17 @@ const oCity1 = document.querySelector('.city-1');
 const oCity2 = document.querySelector('.city-2');
 
 searchBarIcon.addEventListener('click', () => searchBox.classList.toggle('search-box-show'));
+
 btnBack.addEventListener('click', () => searchBox.classList.remove('search-box-show'));
+
 clearInput.addEventListener('click', () => searchInput.value = "");
+
 searchInInput.addEventListener('click', () => {
   searchResults(searchInput.value);
   searchBox.classList.remove('search-box-show');
   searchInput.value = "";
 });
+
 themeSwitcher.addEventListener('click', toggleThemePage);
 function toggleThemePage() {
   document.body.classList.toggle('light-theme');
@@ -38,6 +42,7 @@ function toggleThemePage() {
   backgroundSvg.classList.toggle('bg-light');
   backgroundSvgSearch.classList.toggle('bg-search-box-light');
 };
+
 oCity1.addEventListener('click', () => {
   searchResults("Brasília");
   searchBox.classList.remove('search-box-show');
@@ -49,6 +54,33 @@ oCity2.addEventListener('click', () => {
 
 //========== API CONNECTION ==============//
 
+class Search {
+  constructor() {}
+  async searchCity(city, cityFunctionName) {
+    this.city = city;
+    const response = await fetch(`${api.base}weather?q=${city}&lang=${api.lang}&units=${api.units}&appid=${api.key}`);
+    
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    };
+  
+    const data = await response.json();
+    cityFunctionName(data);
+  }
+  async searchCoords(latitude, longitude, coordinateFunctionName) {
+    this.latitude = latitude;
+    this.longitude = longitude;
+    const response = await fetch(`${api.base}weather?lat=${this.latitude}&lon=${this.longitude}&lang=${api.lang}&units=${api.units}&appid=${api.key}`);
+    
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    } else console.log(response);
+  
+    const data = await response.json();
+    coordinateFunctionName(data);
+  };
+};
+
 const api = {
   key: "f3fb7888586e257660ae9d68ed9afafc",
   base: "https://api.openweathermap.org/data/2.5/",
@@ -57,8 +89,7 @@ const api = {
 };
 
 searchInput.addEventListener('keypress', (event) => {
-  key = event.keyCode;
-  if (key == 13) {
+  if (event.keyCode == 13) {
     searchResults(searchInput.value);
     searchBox.classList.remove('search-box-show');
     searchInput.value = "";
@@ -82,56 +113,33 @@ window.addEventListener('load', () => {
     console.log(error.message);
   };
 });
-const twentySeconds = 20000;
+
+const fourSeconds = 4000;
 setInterval(() => {
   searchBrasilia("Brasília");
   searchPortoAlegre("Porto Alegre");
-}, twentySeconds);
-async function coordsResult(latitude, longitude) {
-  const response = await fetch(`${api.base}weather?lat=${latitude}&lon=${longitude}&lang=${api.lang}&units=${api.units}&appid=${api.key}`);
-  
-  if (!response.ok) {
-    throw new Error(`Error: ${response.status}`);
-  } else console.log(response);
+}, fourSeconds);
 
-  const data = await response.json();
-  showResults(data);
+function coordsResult(latitude, longitude) {
+  const classOfCoordFunction = new Search();
+  classOfCoordFunction.searchCoords(latitude, longitude, showResults);
 };
 
-async function searchBrasilia() {
-  const response = await fetch(`${api.base}weather?q=Brasília&lang=${api.lang}&units=${api.units}&appid=${api.key}`);
-  
-  if (!response.ok) {
-    throw new Error(`Error: ${response.status}`);
-  };
-
-  const data = await response.json();
-  brasiliaResults(data);
+function searchBrasilia(city) {
+  const brasilia = new Search();
+  brasilia.searchCity(city, brasiliaResults);
 };
-
-async function searchPortoAlegre(city) {
-  const response = await fetch(`${api.base}weather?q=${city}&lang=${api.lang}&units=${api.units}&appid=${api.key}`);
-  
-  if (!response.ok) {
-    throw new Error(`Error: ${response.status}`);
-  };
-
-  const data = await response.json();
-  portoAlegreResults(data);
+function searchPortoAlegre(city) {
+  const portoAlegre = new Search();
+  portoAlegre.searchCity(city, portoAlegreResults);
 };
-
-async function searchResults(city) {
-  const response = await fetch(`${api.base}weather?q=${city}&lang=${api.lang}&units=${api.units}&appid=${api.key}`);
-  if (!response.ok) {
-    throw new Error(`Error: ${response.status}`);
-  } else console.log(response);
-  
-  const data = await response.json();
-  showResults(data);
+function searchResults(city) {
+  const anyCity = new Search();
+  anyCity.searchCity(city, showResults);
 };
 
 function showResults(weather) {
-  console.log(weather)
+  console.log(weather);
   city.innerHTML = `${weather.name}, <span id="country">${weather.sys.country}</span>`;
   let description = weather.weather[0].description;
   descriptionOfTemperature.innerHTML = `${description[0].toUpperCase() + description.substr(1)}`;
@@ -145,21 +153,19 @@ function showResults(weather) {
   ilustrationDetailsWeather.src = `/assets/3d weather icons webp/${weather.weather[0].icon}.webp`;
 };
 
-const cityName1 = document.querySelector('#title-o-city-1');
-const ilustrationCity1 = document.querySelector('#img-o-city-1');
-const temperatureCity1 = document.querySelector('#informations-o-city-1');
-
 function brasiliaResults(weather) {
+  const cityName1 = document.querySelector('#title-o-city-1');
+  const ilustrationCity1 = document.querySelector('#img-o-city-1');
+  const temperatureCity1 = document.querySelector('#informations-o-city-1');
   cityName1.innerHTML = `${weather.name}`;
   ilustrationCity1.innerHTML = `/assets/3d weather icons webp/${weather.weather[0].icon}.webp`;
   temperatureCity1.innerHTML = `${Math.round(weather.main.temp)}ºC`;
 };
 
-const cityName2 = document.querySelector('#title-o-city-2');
-const ilustrationCity2 = document.querySelector('#img-o-city-2');
-const temperatureCity2 = document.querySelector('#informations-o-city-2');
-
 function portoAlegreResults(weather) {
+  const cityName2 = document.querySelector('#title-o-city-2');
+  const ilustrationCity2 = document.querySelector('#img-o-city-2');
+  const temperatureCity2 = document.querySelector('#informations-o-city-2');
   cityName2.innerHTML = `${weather.name}`;
   ilustrationCity2.innerHTML = `/assets/3d weather icons webp/${weather.weather[0].icon}.webp`;
   temperatureCity2.innerHTML = `${Math.round(weather.main.temp)}ºC`;
